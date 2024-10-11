a6369e65ba4d24c8150e2ee8b74a499e5cbef660
hajigroups <dhapdhap61@gmail.com>
2024-10-11 15:01:13 +0500

    Update TypedComponent to fix import and enhance functionality

--- app/components/Typed.js
+++ app/components/Typed.js
@@ -2,41 +2,44 @@
 "use client";
 
 import { useEffect } from 'react';
+import Typed from 'typed.js';
 
 const TypedComponent = () => {
   useEffect(() => {
-    import('Typed.js').then(({ default: Typed }) => {
-      const element = document.getElementById('typed-element');
-      if (element) {
-        new Typed(element, {
-          strings: [
-            'Social media marketing',
-            'Content Calendar',
-            'Fb ads',
-            'Video Editing',
-            'Graphic designing',
-            'Content Writing',
-            'Copywriting',
-            'SEO',
-            'Web Development',
-            'Web Designing',
-            'UI\UX Design'
-          ],
-          typeSpeed: 40,       // Speed at which text is typed
-          backSpeed: 20,        // Speed at which text is erased (optional)
-          // backDelay: 700,       // Delay before starting to erase (optional)
-          // startDelay: 500,      // Delay before starting to type (optional)
-          loop: false,          // Loop the typing animation indefinitely
-          showCursor: false,   // Hide the blinking cursor
-         
-        });
-      }
-    });
+    const element = document.getElementById('typed-element');
+    if (element) {
+      const typed = new Typed(element, {
+        strings: [
+          'Social media marketing',
+          'Content Calendar',
+          'Fb ads',
+          'Video Editing',
+          'Graphic designing',
+          'Content Writing',
+          'Copywriting',
+          'SEO',
+          'Web Development',
+          'Web Designing',
+          'UI/UX Design', // Corrected the backslash to a forward slash
+        ],
+        typeSpeed: 40,       // Speed at which text is typed
+        backSpeed: 20,        // Speed at which text is erased (optional)
+        // backDelay: 700,     // Delay before starting to erase (optional)
+        // startDelay: 500,    // Delay before starting to type (optional)
+        loop: false,          // Loop the typing animation indefinitely
+        showCursor: false,    // Hide the blinking cursor
+      });
+
+      // Cleanup function to destroy the instance on component unmount
+      return () => {
+        typed.destroy();
+      };
+    }
   }, []);
 
   return (
     <div
-      className="text-red-500  mt-2 ml-10 text-4xl"
+      className="text-red-500 mt-2 ml-10 text-4xl"
       id="typed-element"
     ></div>
   );
